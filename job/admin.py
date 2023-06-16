2a618b2238f5da531e62bc33b4fb9904d3d22a71
HazemHamdy7 <hazemhamdy1215@gmail.com>
2023-06-16 10:25:20 +0300

    add views job views and urls

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import job
+from .models import job,Categroy
 
 admin.site.register(job)
+admin.site.register(Categroy)
--- job/models.py
+++ job/models.py
@@ -3,9 +3,16 @@ from django.db import models
 JOB_TYPE = (
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
-    
-    
 )
+
+class Categroy(models.Model):
+    name= models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
 class job(models.Model):
     title= models.CharField(max_length=100)
     #location= models
@@ -15,5 +22,7 @@ class job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    categroy = models.ForeignKey(Categroy,on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
